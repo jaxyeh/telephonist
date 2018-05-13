989afbebba2110c06ccf7b88bcfe27b436fc6428
Jason Yeh <jason@yehs.us>
2018-05-13 10:49:13 -0400

    fix mix file

--- mix.exs
+++ mix.exs
@@ -7,9 +7,9 @@ defmodule Telephonist.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     docs: docs,
-     package: package]
+     deps: deps(),
+     docs: docs(),
+     package: package()]
   end
 
   def application do
